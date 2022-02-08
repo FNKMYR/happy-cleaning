095a455f8b014fdb357f4648648111874d438239
Jens Finkemeyer <jens.finkemeyer@outlook.com>
2022-02-08 12:46:21 +0100

    Add Room descriptions

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import './App.css';
 import Room from './Room.js';
+import Header from './Header.js';
 
 export default function App() {
   return (
     <main className="App">
-      <Room text="Küche" isClean />
-      <Room text="Wohnzimmer" isClean={false} />
-      <Room text="Bad" />
+      <Header>Happy cleaning!</Header>
+      <Room
+        text="Küche"
+        descriptionText="Die Spüle muss sauber sein"
+        isDescriptionVisible={true}
+        isClean
+      />
+      <Room
+        text="Wohnzimmer"
+        descriptionText="Das Kuhfell bitte nur mit der Haarrichtung saugen"
+        isDescriptionVisible={false}
+        isClean={false}
+      />
+      <Room
+        text="Bad"
+        descriptionText="Glasreiniger für den Spiegel befindet sich in der Küche"
+        isDescriptionVisible
+      />
     </main>
   );
 }
--- src/Room.js
+++ src/Room.js
@@ -1,12 +1,22 @@
 import './Room.css';
 
-export default function Room({ text, isClean }) {
+export default function Room({
+  text,
+  isClean,
+  descriptionText,
+  isDescriptionVisible,
+}) {
   const statusClassName = `Room__status Room__status--${
     isClean ? 'clean' : 'dirty'
   }`;
+  const description = isDescriptionVisible ? <p>{descriptionText}</p> : '';
   return (
     <section className="Room">
-      {text} <div className={statusClassName}></div>
+      <header className="Room__header">
+        {text}
+        <div className={statusClassName}></div>
+      </header>
+      {description}
     </section>
   );
 }
