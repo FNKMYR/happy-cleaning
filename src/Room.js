86100256365ffc7eae7d93b29b22ff0370c5d9e8
Jens Finkemeyer <jens.finkemeyer@outlook.com>
2022-02-08 11:26:55 +0100

    Add status orb

--- src/Room.js
+++ src/Room.js
@@ -1,9 +1,12 @@
 import './Room.css';
 
 export default function Room({ text, isClean }) {
+  const statusClassName = `Room__status Room__status--${
+    isClean ? 'clean' : 'dirty'
+  }`;
   return (
     <section className="Room">
-      {text} {isClean ? 'Clean' : 'Dirty'}
+      {text} <div className={statusClassName}></div>
     </section>
   );
 }
