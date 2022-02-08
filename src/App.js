eb534a8d4a4bcee258d4eb5ec398e42ecd5798ee
Jens Finkemeyer <jens.finkemeyer@outlook.com>
2022-02-08 14:47:48 +0100

    Add rooms array

--- src/App.js
+++ src/App.js
@@ -3,26 +3,38 @@ import Room from './Room.js';
 import Header from './Header.js';
 
 export default function App() {
+  const rooms = [
+    {
+      text: 'Küche',
+      descriptionText: 'Die Spüle muss sauber sein',
+      isDescriptionVisible: true,
+      isClean: true,
+    },
+    {
+      text: 'Wohnzimmer',
+      descriptionText: 'Das Kuhfell bitte nur mit der Haarrichtung saugen',
+      isDescriptionVisible: false,
+      isClean: false,
+    },
+    {
+      text: 'Bad',
+      descriptionText:
+        'Glasreiniger für den Spiegel befindet sich in der Küche',
+      isDescriptionVisible: true,
+    },
+  ];
   return (
     <main className="App">
       <Header>Happy cleaning!</Header>
-      <Room
-        text="Küche"
-        descriptionText="Die Spüle muss sauber sein"
-        isDescriptionVisible={true}
-        isClean
-      />
-      <Room
-        text="Wohnzimmer"
-        descriptionText="Das Kuhfell bitte nur mit der Haarrichtung saugen"
-        isDescriptionVisible={false}
-        isClean={false}
-      />
-      <Room
-        text="Bad"
-        descriptionText="Glasreiniger für den Spiegel befindet sich in der Küche"
-        isDescriptionVisible
-      />
+      {rooms.map(({ text, descriptionText, isDescriptionVisible, isClean }) => (
+        <Room
+          key={text}
+          text={text}
+          descriptionText={descriptionText}
+          isDescriptionVisible={isDescriptionVisible}
+          isClean={isClean}
+        />
+      ))}
     </main>
   );
 }
